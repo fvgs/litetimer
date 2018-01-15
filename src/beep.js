fa13fed66d0c4af13765004d1cf29262dd9310d9
Felipe Vargas <git@fvgs.ai>
2018-01-15 04:25:48 -0500

    Output bell and error message if audio playback fails

--- src/beep.js
+++ src/beep.js
@@ -1,14 +1,15 @@
 const {join, dirname} = require('path')
 const Player = require('play-sound')
 
-const {stdin} = process
+const {stdin, stdout} = process
 const audio = join(dirname(__dirname), 'haiku.flac')
 
 const beep = () => {
 	const player = Player()
 	beep.process = player.play(audio, err => {
 		if (err) {
-			throw err
+			stdout.write('\x07')
+			console.log('Error: Unable to play audio')
 		}
 		stdin.pause()
 	})
